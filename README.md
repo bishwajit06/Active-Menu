964617ceecca5baa25b4431f96fd3b0d6a1c5c17
Bishwajit Roy <roybishwajit06@gmail.com>
2020-02-27 01:01:56 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This a Active Link create by jQuery
---
+++ custom.js
@@ -0,0 +1,5 @@
+
+  $('.category').on('click','a',function(){
+	$('.category a.active').removeClass('active');
+	$(this).addClass('active');
+  });
